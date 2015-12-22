7024a245f5fec65661f76af6af44c190395fc129
Lawrence Allan Jones <lawrence.jones@openlink.com>
2015-12-22 13:00:25 -0600

    Add word-source module.
    Copy the word-source module from the word-source-cljs project. Change it
    to fit as a library. This change allows me to call it's functions within
    a node REPL.
    
    To run it, one must execute the command `npm install sprintf-js`;
    however, I do not believe I actually need this node package for this
    module.

---
+++ src/test_utils/word_source.cljs
@@ -0,0 +1,41 @@
+(ns test-utils.word-source
+  (:require [cljs.nodejs :as nodejs])
+  (:use [clojure.string :only [split, split-lines]])
+  )
+
+(def fs (js/require "fs"))
+
+(def sprintf (js/require "sprintf-js"))
+
+(nodejs/enable-util-print!)
+
+(defn read-file [name callback]
+  (.readFile fs name "utf-8" callback))
+
+(defn str->word-definitions 
+  ([text]
+   (->> (clojure.string/split-lines text)
+        (partition 2))
+   ))
+
+(defn when-file-read [err data]
+  (if err
+    (println err)
+    (let [definitions (str->word-definitions data)
+          selected-definition (nth definitions (rand-int (count definitions)))]
+      (println selected-definition))))
+
+(defn dump-em [err data] 
+  (if err
+    (println "Error:" err)
+    (println "Data:" data)))
+
+(defn next-word []
+  (read-file "latin_words.txt" when-file-read))
+
+;; (defn -main [& args]
+;;   (println "Hello world!")
+;;   (read-file "latin_words.txt" when-file-read))
+
+;; (set! *main-cli-fn* -main)
+
