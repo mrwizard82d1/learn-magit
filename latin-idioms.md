ebbe5edd000b29535dc3a6b742be0b87fee01392
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2016-01-31 20:44:41 -0600

    Add word-source module to test-utils.
    Add module to test-utils that generates random (Latin) words for
    testing. Copied latin_words.txt and latin-idioms.txt from word-source
    project. Rename files to latin-words.md and latin-idioms.md,
    respectively. Edit latin-words.md and latin-idioms.md to better conform
    to Markdown definitions (pandoc). Implement methods:
    
    - `read-text-file`: reads a text file from disk
    - `parse-lines`: splits a string into lines
    - `parse-definitions`: parse (Markdown) word definitions from a sequence
      of lines.
    - `read-latin-words-from`: Reads Latin words with definitions from a
      file at pathname.
    - `read-latin-words`: A memoized function for reading Latin words and
      definitions from a file.
    - `rand-words`: The public API for reading a random Latin word.

---
+++ latin-idioms.md
@@ -0,0 +1,38 @@
+# Latin Idioms
+
+amabo te  IDIOM
+:   please
+
+poenas dare  IDIOM
+:   to pay the penalty
+
+basiare te  IDIOM
+:   to kiss you
+
+sub rosa  IDIOM
+:   in secret
+
+aqua vitae  IDIOM
+:   water of life
+
+sine + cura  IDIOM
+:   without care (cf. sinecure: a senior position without responsibilities)
+
+quinta essentia  IDIOM
+:   fifth essence
+
+prae sequor  IDIOM
+:   go on before
+
+absconsa lantern  IDIOM
+:   hidden (dark) lantern
+
+lapidat  IDIOM
+:   it rains stones
+
+in gratiam  IDIOM
+:   into favor
+
+ad frontem  IDIOM
+:   to the face
+
---
+++ src/test_utils/word_source.cljs
@@ -0,0 +1,41 @@
+(ns test-utils.word-source
+   (:require [cljs.nodejs :as nodejs]
+             [clojure.string :as clj-str]))
+
+(def fs (js/require "fs"))
+
+(defn read-text-file [pathname]
+  (.readFileSync fs pathname "utf-8"))
+
+(defn empty-string? [to-test]
+  "Determine if to-test is an empty string."
+  (and (string? to-test)
+       (== (.-length to-test) 0)))
+
+(defn parse-lines [text]
+  (clj-str/split text #"\n"))
+
+(defn empty-string-sequence? [to-test]
+  "Determine if to-test is a sequence containing only an empty string."
+  (and (= 1 (count to-test))
+       (empty-string? (first to-test))))
+
+(defn parse-definitions [lines]
+  (->> lines
+       (drop 2)
+       (map #(.trim %))
+       (partition-by empty-string?)
+       (remove empty-string-sequence?)))
+
+(defn read-latin-words-from [pathname]
+  (let [definitions (-> pathname
+                        (read-text-file)
+                        (parse-lines)
+                        (parse-definitions))]
+    (map #(str (first %) (second %)) definitions)))
+
+(def read-latin-words (memoize read-latin-words-from))
+
+(defn rand-words
+  ([] (rand-words (read-latin-words "latin-words.md") rand-nth))
+  ([words rand-f] (rand-f words)))
