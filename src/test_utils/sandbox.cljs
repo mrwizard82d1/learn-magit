5d01a01d2685a9d5a999cca7b797e89acca613c7
Lawrence Allan Jones <lawrence.jones@openlink.com>
2016-05-26 16:24:28 -0500

    Added "sandbox" module for common usages.

---
+++ src/test_utils/sandbox.cljs
@@ -0,0 +1,23 @@
+(ns test-utils.sandbox
+  (:require [test-utils.core :as tuc] 
+            [test-utils.word-source :as ws]))
+
+;; Stuff I'm trying out
+
+(defn name-abbreviation-pair []
+  (repeatedly 3 ws/rand-word))
+
+(defn person-name []
+  [(ws/rand-word) (if (= 0 (rem (rand-int 300) 3)) (tuc/rand-alpha) "") (ws/rand-word)])
+
+(defn house-number []
+  (tuc/rand-4))
+
+(defn street-name []
+  (repeatedly 2 ws/rand-word))
+
+(defn zip-code []
+  (tuc/rand-5))
+
+(defn phone-number []
+  (str "555-" (tuc/rand-3) "-" (tuc/rand-4)))
