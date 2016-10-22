4f73cc3676dd580db2b39f78f30656b45b7c3b95
Larry Jones <larryj@blinds.com>
2016-10-22 08:49:49 -0500

    Updated versions of Clojure / ClojureScript.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject test-utils-cljs "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.122" :classifier "aot"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.8.51" :classifier "aot"
                   :exclusion [org.clojure/data.json]]
                  [org.clojure/data.json "0.2.6" :classifier "aot"]]
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
