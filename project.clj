145f6df946bb8d2f88cc03f0135c1268545a848e
Larry Jones <larry.jones@reveal-energy.com>
2018-04-18 15:53:37 -0500

    Upgrade Clojure (1.9.0) / ClojureScript (1.9.946)

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
 (defproject test-utils-cljs "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.8.51" :classifier "aot"
-                  :exclusion [org.clojure/data.json]]
-                 [org.clojure/data.json "0.2.6" :classifier "aot"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.9.946" :classifier "aot"]
+                 [kixi/stats "0.4.0"]]
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :plugins [[lein-npm "0.6.1"]]
   :npm {:dependencies [[source-map-support "0.3.2"]]}
