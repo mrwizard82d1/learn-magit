cf2cf0e1b1f1c6f3452f486f86d154e268fa0eed
Larry Jones <larry.jones@reveal-energy.com>
2020-03-27 20:00:20 -0500

    Clojure targeting Java

--- project.clj
+++ project.clj
@@ -1,12 +1,8 @@
-(defproject test-utils-cljs "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
+(defproject test-utils "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.9.946" :classifier "aot"]
-                 [kixi/stats "0.4.0"]]
-  :jvm-opts ^:replace ["-Xmx1g" "-server"]
-  :plugins [[lein-npm "0.6.1"]]
-  :npm {:dependencies [[source-map-support "0.3.2"]]}
-  :source-paths ["src" "target/classes"]
-  :clean-targets ["out" "release"]
-  :target-path "target")
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :repl-options {:init-ns test-utils.core}
+  :source-paths ["src", "test"])
